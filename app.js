573808c8a4d9542c2b430a77466b04f5ee755080
RakshithBhat03 <rakshith6666@gmail.com>
2021-08-16 22:59:54 +0530

    added functionalities

---
+++ app.js
@@ -0,0 +1,29 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var inputText = document.querySelector("#userInput");
+var outputText = document.querySelector("#translatedOutput");
+var url = "https://api.funtranslations.com/translate/pirate.json";
+btnTranslate.addEventListener("click", clickHandler);
+btnTranslate.addEventListener("mouseover", () => {
+  btnTranslate.textContent = "☠️Translate!☠️";
+});
+btnTranslate.addEventListener("mouseout", () => {
+  btnTranslate.textContent = "Translate!";
+});
+function clickHandler(event) {
+  var message = inputText.value;
+  var finalUrl = urlHandler(message);
+  console.log(finalUrl);
+  fetch(finalUrl)
+    .then((response) => response.json())
+    .then((json) => {
+      outputText.innerText = json.contents.translated;
+    })
+    .catch(() => {
+      {
+        alert("Error occured");
+      }
+    });
+}
+function urlHandler(text) {
+  return url + "?text=" + text;
+}
